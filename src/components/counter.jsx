2b27c622fe560b2df17974fa0753edcfcefeba1e
Christian Levin <christian.johansson@gmail.com>
2018-10-17 13:34:23 +0200

    complete code along

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  componentDidUpdate(prevProps, prevState) {
+    console.log("prevProps", prevProps);
+    console.log("prevState", prevState);
+    if (prevProps.counter.value !== this.props.counter.value) {
+      //Ajax call and get new data
+    }
+  }
+
+  componentWillUnmount() {
+    console.log("Counter - Unmount");
+  }
   /* state = {
     value: this.props.counter.value
     // tags: ["tag1", "tag2", "tag3"]
@@ -28,6 +39,7 @@ class Counter extends Component {
   }; */
 
   render() {
+    console.log("Counter - Rendered");
     return (
       <div>
         {/* {this.state.tags.length === 0 && "please create a new tag"} */}
---
+++ src/components/navbar.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+//statless Functional Component
+const NavBar = ({ totalCounters }) => {
+  console.log("NavBar - Rendered");
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
+
+export default NavBar;
+/* 
+class NavBar extends Component {
+  render() {}
+} */
+
+//export default NavBar;
